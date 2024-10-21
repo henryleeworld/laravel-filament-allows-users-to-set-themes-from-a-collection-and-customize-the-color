a8813bf087f6a6dce75e6e2d3cecc84d0814b033
Henry Lee <henryleeworld@gmail.com>
2024-10-22 00:33:59 +0800

    feat:初始化

---
+++ lang/vendor/themes/zh_TW/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => '主題',
+    'primary_color' => '主要顏色',
+    'select_base_color' => '為您的主題選擇基礎顏色。',
+    'custom' => '自訂。',
+    'no_changing_primary_color' => '這些主題不支援變更主要顏色。',
+    'support_changing_primary_color' => '這些主題支援變更主要顏色。',
+    'select_interface' => '選擇您希望介面的外觀。',
+    'select' => '選擇',
+    'light' => '淺色',
+    'dark' => '深色',
+    'active' => '有效的',
+    'primary_color_set' => '主題主要顏色設定為',
+    'theme_set_to' => '主題設定為',
+    'appearance' => '外觀',
+    'no_light_mode' => '此主題不支援淺色模式。',
+    'no_dark_mode' => '此主題不支援深色模式。',
+    'support_light_mode' => '此主題支援淺色模式。',
+    'support_dark_mode' => '此主題支援深色模式。',
+    'theme_active' => '該主題目前處於有效狀態。',
+];
